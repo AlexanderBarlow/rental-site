a4973c3f0494d4dd65a64184d40539ebbe026a94
Jamie <asapharris88@gmail.com>
2023-01-31 13:57:16 -0500

    creating pages and components for Profile and checkout pages

--- my-app/client/src/pages/CheckoutPage.js
+++ my-app/client/src/pages/CheckoutPage.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import CheckoutCard from '../components/CheckoutCard';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import './CheckoutPage.css';
+import React from "react";
+import CheckoutCard from "../components/Checkout/CheckoutCard";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./CheckoutPage.css";
 
 function CheckoutPage() {
   return (
     <div className="Checkout">
       <Navbar />
-        <CheckoutCard />
+      <CheckoutCard />
       <Footer />
     </div>
   );
--- my-app/client/src/pages/ProfilePage.js
+++ my-app/client/src/pages/ProfilePage.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import CheckoutCard from '../components/ProfileContactCard';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import './ProfilePage.css';
+import React from "react";
+import ProfileContactCard from "../components/Profile/ProfileContactCard";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./ProfilePage.css";
 
 export default function ProfilePage() {
-    return (
-        <div>
-            <Navbar />
-        <ProfileContactCard />
+  return (
+    <div>
+      <Navbar />
+      <ProfileContactCard />
       <Footer />
-        </div>
-    );
+    </div>
+  );
 }
