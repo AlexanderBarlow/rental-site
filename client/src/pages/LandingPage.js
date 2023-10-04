6100d798abbb7b9b7701d7116d550a6c376e14c1
Alexander Barlow <alex.barlow@rocketmail.com>
2023-10-04 13:57:00 -0400

    adding new styling to the landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -2,57 +2,54 @@ import React from "react";
 import 'materialize-css/dist/css/materialize.min.css';
 import LOGO from "../images/logo.png";
 
+
 const styles = {
-    h1: {
-        color: '#00A6FB',
-        fontFamily: '',
-        padding: '50px',
-        textAlign: 'center',
-    },
-    height: {
-      height: '100vh',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    container: {
-        justifyContent: 'center',
-        height: '1000px',
-        alignItems: 'center',
-        padding: '10px',
-        display: 'flex',
-        border: '2px solid #000',
-        boxShadow: '5px 10px 10px #00A6FB',
-        height: '50%',
-        background: '#051923',
-    },
-    // content: {
-    // },
-    img: {
-      width: '50%'
-    },
-    p: {
-      color: '#00A6FB',
-      textShadow: '5px 5px 5px #000'
-    },
-    flex: {
-      display: 'flex'
-    }
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    padding: '20px',
+    width: '100%',
+  },
+  content: {
+    textAlign: 'center',
+    maxWidth: '80%', // Adjust the maximum width to 80%
+    margin: '0 auto',
+    color: '#00A6FB',
+    textShadow: '5px 5px 5px #000',
+    background: '#051923',
+    padding: '20px', // Add padding to the content
+  },
+  imgContainer: {
+    display: 'flex',
+    flexDirection: 'column', // Change to column layout on smaller screens
+    alignItems: 'center',
+    marginTop: '20px',
+  },
+  img: {
+    width: '50%',
+    marginBottom: '20px', // Add spacing below the image
+  },
+  text: {
+    fontSize: '18px', // Increase the text font size
+    margin: '10px 0', // Add vertical margin to the text
+  },
 };
 
 export default function LandingPage() {
   return (
-    <div style={styles.height}>
-      <div className="container" style={styles.container}>
-        <div style={styles.content}>
+    <div style={styles.container}>
+      <div id="card" style={styles.content}>
         <img src={LOGO} alt='logo' style={styles.img}></img>
-        <p style={styles.p}>
-          Login or Sign up to rent products or services near you! Want to start
-          making some extra dough! You too can add your items to the market
-          place.
-        </p>
+        <div style={styles.imgContainer}>
+          <p style={styles.text}>
+            Login or Sign up to rent products or services near you! Want to start
+            making some extra dough! You too can add your items to the marketplace.
+          </p>
+          <img src='Hotpot.png' alt='' style={styles.img}></img>
         </div>
-        <img src="Hotpot.png" alt='random generated art' style={styles.img}></img>
       </div>
     </div>
   );
