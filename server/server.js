dfc3cdff7a1aba6c2593da9863e97c3677c645d8
Alexander Barlow <alex.barlow@rocketmail.com>
2024-03-26 14:33:11 -0400

    server connection

--- server/server.js
+++ server/server.js
@@ -13,10 +13,6 @@ const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Enable CORS for your frontend domain
-app.use(cors({
-  origin: ["https://nestease.vercel.app", "http://localhost:3000"]
-}));
 
 const server = new ApolloServer({
   typeDefs,
