0014985a4cce241b0194a4a58ae86fba695bbda1
Alexander Barlow <alex.barlow@rocketmail.com>
2024-03-26 14:36:03 -0400

    server connection

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ import AddCreddit from "./pages/AddCredit";
 import EditProfile from "./pages/EditProfile";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("id_token");
